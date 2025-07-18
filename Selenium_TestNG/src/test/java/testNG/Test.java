df5a5edcfa37f2297f1dc1449b64cd131a989c33
Jaiwant Toppo <jaiwant.toppo@esri.in>
2025-07-18 16:46:27 +0530

    second commit

--- Selenium_TestNG/src/test/java/testNG/Test.java
+++ Selenium_TestNG/src/test/java/testNG/Test.java
@@ -3,6 +3,9 @@ package testNG;
 public class Test {
 	
 	public void test() {
-		System.out.println("hi");
+		System.out.println("hi")}
+
+	public void test2() {
+		System.out.println("Hello World!");
 	}
 }
